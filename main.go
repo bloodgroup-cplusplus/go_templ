38cf4a9b1472b12d76f378e8fa08ffbf26b3c2cb
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-04-17 08:15:30 +0530

    adding new values

--- main.go
+++ main.go
@@ -7,9 +7,25 @@ import (
 	"github.com/a-h/templ"
 )
 
+type Data struct {
+	message string
+}
+templ (d Data) Method() {
+	<div>{d.message}</div>
+}
+templ Message(){
+	<div>
+	@Data{
+		message:"you can implement method"
+	}.Method()
+	</div>
+}
+
 func main() {
-	component := hello("John")
-	http.Handle("/", templ.Handler(component))
-	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+//	component := hello("John")
+//	http.Handle("/", templ.Handler(component))
+//	fmt.Println("Listening on :3000")
+//	http.ListenAndServe(":3000", nil)
+Message().Render(context.Background(), os.Stdout)
+}
 }

ae5cf553c113b17505418c5809c3334b7058f4a3
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-20 18:34:25 -0400

    uploading files

--- gohtmxcrud/main.go
+++ gohtmxcrud/main.go
@@ -25,6 +25,22 @@ func main() {
 // Middleware setup
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	// middleware for cors
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:[]string,
+		AllowedMethods:[]string{"GET","POST","PUT","DELETE","OPTIONS"},
+	}))
+
+	// create uploads dirctory for storing images
+	os.MkdirAll("uploads",os.ModePerm)
+
+	r.Handle("/uploads/*",http.StripPrefix("/uploads",http.FileServer
+	((http.Dir("uploads")))))
+
+	// Route definitions
+
+		
+
 
 
 

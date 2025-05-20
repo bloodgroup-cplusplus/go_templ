42fb00b6471bd53b3e69b35875346913ca57f30e
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-20 18:44:49 -0400

    adding cors library

--- gohtmxcrud/main.go
+++ gohtmxcrud/main.go
@@ -4,6 +4,10 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/middleware"
+	"net/http"
+	"os"
+	"fmt"
+	"log"
 )
 
 type Item struct {
@@ -30,15 +34,20 @@ func main() {
 		AllowedOrigins:[]string,
 		AllowedMethods:[]string{"GET","POST","PUT","DELETE","OPTIONS"},
 	}))
-
 	// create uploads dirctory for storing images
 	os.MkdirAll("uploads",os.ModePerm)
-
 	r.Handle("/uploads/*",http.StripPrefix("/uploads",http.FileServer
 	((http.Dir("uploads")))))
-
 	// Route definitions
+	// 1 GET server the main/home page
+	// 2 GET Return list of items
+	// 3 POST Create a new item
+	// 4 DELTE delte an item with an ID
+	// Start the server 
+	fmt.Println("Server starting on http: locahost:300")
+	log.Fatal(http.ListenAndServe(":3000",r))
 
+}
 		
 
 

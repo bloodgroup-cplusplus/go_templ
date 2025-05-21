5a2c2977b0aecb99a2f9f38ba290b398d1e27bc7
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-21 14:38:26 -0400

    shows current time with htmx interactivity

--- templ/main.go
+++ templ/main.go
@@ -3,13 +3,14 @@ import (
 	"net/http"
 	"fmt"
 	"html/template"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl,err := template.ParseFiles("index.html")
 		if err != nil {
-			http.Error(w, err.Error(),http.StatusInternalServerError)
+			http.Error(w, err.Error(),http.StatusInternalServerError)			
 			return 
 		}
 		// anonymous structs
@@ -20,6 +21,11 @@ func main() {
 		}
 		tmpl.Execute(w,data)
 			
+	})
+
+	http.HandleFunc("/time",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([] byte("Current time:"+ time.Now().Format(time.RFC1123)))
+		
 	})
 
 	fmt.Println("We started our http server on port :8080")

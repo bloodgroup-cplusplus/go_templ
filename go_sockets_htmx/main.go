f616ed7aa17b2e386f2b8faa897ec8a575ab8d78
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-21 17:57:48 -0400

    adding main file

---
+++ go_sockets_htmx/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func serveIndex( w http.ResponseWriter, r * http.Request) {
+}
+
+
+func main()  {
+	http.HandleFunc("/",serveIndex)
+	http.HandleFunc("/ws",nil)
+	log.Fatal(http.ListenAndServe(":3000",nil)
+}

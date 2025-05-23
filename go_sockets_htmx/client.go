5a07ccc3c56c2609258c690dd137dbcc41d47eff
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-22 21:25:08 -0400

    adding client features

--- go_sockets_htmx/client.go
+++ go_sockets_htmx/client.go
@@ -5,11 +5,33 @@ import (
 "github.com/gorilla/websocket"
 )
 
+type Message struct {
+	ClientID string
+	Text string
+}
+
 type Client struct {
+	id string
 	hub *Hub
 	conn *websocket.Conn
 	send chan []byte
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:1024,
+	WriteBufferSize:1024,
+
 func serveWs(hub *Hub, w http.ResponseWriter,r *http.Request) {
+	// upgrade the conn
+	// create client
+	// listening the hbu
+	conn, err := upgrader.Upgrade(w,r,nil)
+	if err != nil {
+		log.Println(err)
+		return 
+	}
+	client := &Client{hub:hub,conn:conn, send:make(chan[]byte)}
+
+
+
 }
 

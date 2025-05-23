d6da68bbb86cf0fd661410b39f97d2f575c3244d
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-22 21:15:35 -0400

    adding client and hub respectively

--- go_sockets_htmx/main.go
+++ go_sockets_htmx/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func serveIndex( w http.ResponseWriter, r * http.Request) {
-	if r.URL !="/"{
+	if r.URL.Path != "/" {
 		http.Error(w,"not found",http.StatusNotFound)
 		return 
 	}
@@ -15,12 +16,16 @@ func serveIndex( w http.ResponseWriter, r * http.Request) {
 		return 
 	}
 
-	http.ServeFile(w,"templates/index.html")
+	http.ServeFile(w,r,"templates/index.html")
 }
 
 
 func main()  {
 	http.HandleFunc("/",serveIndex)
-	http.HandleFunc("/ws",nil)
-	log.Fatal(http.ListenAndServe(":3000",nil)
+	http.HandleFunc("/ws",func (w http.ResponseWriter, r *http.Request ) {
+		serveWs(hub,w,r)
+	})
+
+	fmt.Println("Server running on port 3000")
+	log.Fatal(http.ListenAndServe(":3000",nil))
 }

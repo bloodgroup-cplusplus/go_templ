d59c44a3c629f264502b82ed191b9da21f943ef4
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-22 21:19:35 -0400

    adding the gorilla websocket package

--- go_sockets_htmx/client.go
+++ go_sockets_htmx/client.go
@@ -2,7 +2,13 @@ package main
 
 import (
 "net/http"
+"github.com/gorilla/websocket"
 )
+
+type Client struct {
+	hub *Hub
+	conn *websocket.Conn
+	send chan []byte
 
 func serveWs(hub *Hub, w http.ResponseWriter,r *http.Request) {
 }
--- go_sockets_htmx/hub.go
+++ go_sockets_htmx/hub.go
@@ -1,5 +1,10 @@
 package main
 
 type Hub struct {
+	clients map[*Client] bool 
+
+	broadcast chan *Message
+	register chan *Client
+	unregister chan *Client
 
 }
